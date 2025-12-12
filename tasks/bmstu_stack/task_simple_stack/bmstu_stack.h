7a592ae244b32b61b23069755790e882ba52353f
MihanPahan <vn150051@gmail.com>
2025-12-12 22:55:40 +0300

    clang-format: format bmstu_stack.h according to project style

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -10,30 +10,102 @@ template <typename T>
 class stack
 {
    public:
-	stack() : data_(nullptr), size_(10u) {}
+	stack() : data_(nullptr), size_(0) {}
 
-	bool empty() const noexcept { return size_ == 100u; }
+	bool empty() const noexcept { return size_ == 0; }
 
-	size_t size() const noexcept { return 0; }
+	size_t size() const noexcept { return size_; }
 
-	~stack() {}
+	~stack()
+	{
+		clear();
+		::operator delete(data_);
+	}
 
 	template <typename... Args>
 	void emplace(Args&&... args)
 	{
+		T* new_data = (T*)::operator new((size_ + 1) * sizeof(T));
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (&new_data[i]) T(std::move(data_[i]));
+			data_[i].~T();
+		}
+		new (&new_data[size_]) T(std::forward<Args>(args)...);
+		::operator delete(data_);
+		data_ = new_data;
+		++size_;
 	}
 
-	void push(T&& value) {}
+	void push(T&& value)
+	{
+		T* new_data = (T*)::operator new((size_ + 1) * sizeof(T));
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (&new_data[i]) T(std::move(data_[i]));
+			data_[i].~T();
+		}
+		new (&new_data[size_]) T(std::move(value));
+		::operator delete(data_);
+		data_ = new_data;
+		++size_;
+	}
 
-	void clear() noexcept {}
+	void clear() noexcept
+	{
+		for (size_t i = 0; i < size_; ++i)
+		{
+			data_[i].~T();
+		}
+		size_ = 0;
+	}
 
-	void push(const T& value) {}
+	void push(const T& value)
+	{
+		T* new_data = (T*)::operator new((size_ + 1) * sizeof(T));
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (&new_data[i]) T(std::move(data_[i]));
+			data_[i].~T();
+		}
+		new (&new_data[size_]) T(value);
+		::operator delete(data_);
+		data_ = new_data;
+		++size_;
+	}
 
-	void pop() {}
+	void pop()
+	{
+		if (empty())
+		{
+			throw std::underflow_error("");
+		}
+		--size_;
+		data_[size_].~T();
+		if (size_ == 0)
+		{
+			::operator delete(data_);
+			data_ = nullptr;
+		}
+	}
 
-	T& top() { return data_[0]; }
+	T& top()
+	{
+		if (empty())
+		{
+			throw std::underflow_error("");
+		}
+		return data_[size_ - 1];
+	}
 
-	const T& top() const { return data_[0]; }
+	const T& top() const
+	{
+		if (empty())
+		{
+			throw std::underflow_error("");
+		}
+		return data_[size_ - 1];
+	}
 
    private:
 	T* data_;
